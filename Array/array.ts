767794661fb82eb8e039cc3fd578ce0930c1aba2
SFJT-97 <santiagojuarez.dev@gmail.com>
2024-03-16 10:39:20 -0400

    Array!

---
+++ Array/array.ts
@@ -0,0 +1,6 @@
+const words1: string[] = [];
+const words2: string[] = ["World!", "just", "started."];
+
+words1.push("Hello", "I'm", "getting");
+
+console.log(words1[0], words2[0], words1[1], words2[1], words1[2], words2[2], words1[3], words2[3])
