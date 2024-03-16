ce91470c1b62a3cca210fd7ddf1e297ab28dd8a2
SFJT-97 <santiagojuarez.dev@gmail.com>
2024-03-16 10:12:31 -0400

    Hello World!

---
+++ hello.js
@@ -0,0 +1,2 @@
+var helloworld = "Hello World! I'm just getting started";
+console.log(helloworld);
---
+++ hello.ts
@@ -0,0 +1,4 @@
+let helloworld: string = "Hello World! I'm just getting started"
+console.log(helloworld)
+
+/* To run, write "ts-node hello.ts" on the terminal */
